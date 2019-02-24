9b3887cc199d03d295b3ab0d0745e9d4a065b31a
toodef <fedotov.anton.af@gmail.com>
2019-02-25 00:16:51 +0300

    try to add ci

--- tools/build_lib.bat
+++ tools/build_lib.bat
@@ -6,6 +6,6 @@ if not exist "python" mkdir python && cd python
 
 cmake ..\.. -G "Visual Studio 15 2017 Win64" -DBUILD_EXAMPLES="ON" -DBUILD_PYTHON="OFF" -DVERSION="0.0.2" || exit /b 1
 
-msbuild /nologo /verbosity:quiet /p:Configuration=Release cpp-template.sln || exit /b 1
+msbuild /nologo /verbosity:quiet /p:Configuration=Release light-engine.sln || exit /b 1
 
 cd ..\..\..
--- tools/build_python.bat
+++ tools/build_python.bat
@@ -1,8 +1,14 @@
-@echo off
+REM @echo off
 
 if not exist "artifacts" mkdir && cd artifacts
 if not exist "win" mkdir win && cd win
 if not exist "lib" mkdir lib && cd lib
+
+git clone https://github.com/pybind/pybind11.git
+
+dir
+
+set PYBIND11_DIR=pybind11
 
 cmake ..\.. -G "Visual Studio 15 2017 Win64" -DBUILD_EXAMPLES="OFF" -DBUILD_PYTHON="ON" -DVERSION="0.0.2" || exit /b 1
 
