3b369652e181cadc17f12fa1003d29fbeb4b2079
toodef <fedotov.anton.af@gmail.com>
2019-02-25 01:13:36 +0300

    try to add ci

--- tools/build_lib.bat
+++ tools/build_lib.bat
@@ -4,10 +4,13 @@ if not exist "artifacts" mkdir artifacts && cd artifacts
 if not exist "win" mkdir win && cd win
 if not exist "lib" mkdir lib && cd lib
 
+curl -OL https://github.com/nigels-com/glew/releases/download/glew-2.1.0/glew-2.1.0-win32.zip -o glew-2.1.0-win32.zip
+7z e glew-2.1.0-win32.zip
+
 dir
+dir glew-2.1.0-win32
 
 cmake ..\..\.. -G "Visual Studio 15 2017 Win64" -DBUILD_EXAMPLES="ON" -DBUILD_PYTHON="OFF" -DVERSION="0.0.2" || exit /b 1
-
 msbuild /nologo /verbosity:quiet /p:Configuration=Release Project.sln || exit /b 1
 
 cd ..\..\..
