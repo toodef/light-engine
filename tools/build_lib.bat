3d634cf2727f69ebdd6858dad270b5870f4f638f
toodef <fedotov.anton.af@gmail.com>
2019-02-25 00:12:09 +0300

    try to add ci

--- tools/build_lib.bat
+++ tools/build_lib.bat
@@ -4,7 +4,7 @@ if not exist "artifacts" mkdir && cd artifacts
 if not exist "win" mkdir win && cd win
 if not exist "python" mkdir python && cd python
 
-cmake ..\..\.. -G "Visual Studio 15 2017 Win64" -DBUILD_EXAMPLES="ON" -DBUILD_PYTHON="OFF" -DVERSION="0.0.2" || exit /b 1
+cmake ..\.. -G "Visual Studio 15 2017 Win64" -DBUILD_EXAMPLES="ON" -DBUILD_PYTHON="OFF" -DVERSION="0.0.2" || exit /b 1
 
 msbuild /nologo /verbosity:quiet /p:Configuration=Release cpp-template.sln || exit /b 1
 
