5bcb0022c827f5c777f304441f90ac4d5ba234ec
toodef <fedotov.anton.af@gmail.com>
2019-02-25 23:52:28 +0300

    try to add ci

--- tools/build_lib.bat
+++ tools/build_lib.bat
@@ -6,7 +6,7 @@ if not exist "lib" mkdir lib && cd lib
 
 call ..\..\..\tools\install_deps_lib.bat
 
-cmake ..\..\.. -G "Visual Studio 15 2017 Win64" -DBUILD_EXAMPLES="ON" -DBUILD_PYTHON="OFF" -DVERSION="0.0.2" || exit /b 1
+cmake ..\..\.. -G "Visual Studio 15 2017 Win64" -DBUILD_EXAMPLES="OFF" -DBUILD_PYTHON="OFF" -DVERSION="0.0.2" || exit /b 1
 msbuild /nologo /verbosity:quiet /p:Configuration=Release Project.sln || exit /b 1
 
 cd ..\..\..
