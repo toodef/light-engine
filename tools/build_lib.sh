814190890e9d3a36bedb7d96d0e9c276fb463f77
Anton Fedotov <fedotov.anton.af@gmail.com>
2019-02-28 15:42:04 +0300

    try to add ci

--- tools/build_lib.sh
+++ tools/build_lib.sh
@@ -3,7 +3,7 @@
 mkdir build
 cd build
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=installed_lib ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=installed_lib -DBUILD_EXAMPLES="OFF" -DBUILD_PYTHON="OFF" -DVERSION="0.0.2" ..
 make
 
 cd ..
